a702a21ab3c55c44cbe69111916fa9083667648e
doicrm <michalakradek2@gmail.com>
2023-03-23 16:11:36 +0100

    Created structure of save manager class

---
+++ src/SaveManager.cs
@@ -0,0 +1,56 @@
+﻿namespace Nocturnal.src
+{
+    public struct SaveData
+    {
+        string Date;
+        string Hour;
+        string Player;
+        int Chapter;
+        int Gender;
+        int Stage;
+    };
+
+    internal class SaveManager
+    {
+        public static void CreateSave()
+        {
+
+        }
+
+        public static void LoadSave(int nr)
+        {
+
+        }
+
+        public static void UpdateSave(int saveNr, string player, int sex, int chapter, int stage)
+        {
+
+        }
+
+        public static void PrintSavesList()
+        {
+
+        }
+
+        public static string PrintSex(int sex)
+        {
+
+        }
+
+        private static string GetChapterString(int chapter)
+        {
+
+        }
+
+        private static void LoadSaveInfo(string saveToLoad)
+        {
+
+        }
+
+        private static void SearchForSaves()
+        {
+
+        }
+
+    }
+}
