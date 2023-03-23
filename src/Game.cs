276bb2da3b38131476c4b0c892c442148508c4eb
doicrm <michalakradek2@gmail.com>
2023-03-23 16:10:12 +0100

    Updated game classes

---
+++ src/Game.cs
@@ -0,0 +1,174 @@
+﻿namespace Nocturnal.src
+{
+    public enum Weather { Sunny, Cloudy, Stormy, Rainy, Snowfall }
+
+    internal class Game
+    {
+        public static readonly Game Instance = new();
+        private bool IsPlaying { get; set; }
+        private int Menu { get; set; }
+        private int Choice { get; set; }
+        private Location? CurrentLocation { get; set; }
+        private Weather Weather { get; set; }
+
+        public static readonly string[] Logo = new string[8] {
+            "\t ****     **   *******     ******  ********** **     ** *******   ****     **     **     **\n",
+            "\t/**/**   /**  **/////**   **////**/////**/// /**    /**/**////** /**/**   /**    ****   /**\n",
+            "\t/**//**  /** **     //** **    //     /**    /**    /**/**   /** /**//**  /**   **//**  /**\n",
+            "\t/** //** /**/**      /**/**           /**    /**    /**/*******  /** //** /**  **  //** /**\n",
+            "\t/**  //**/**/**      /**/**           /**    /**    /**/**///**  /**  //**/** **********/**\n",
+            "\t/**   //****//**     ** //**    **    /**    /**    /**/**  //** /**   //****/**//////**/**\n",
+            "\t/**    //*** //*******   //******     /**    //******* /**   //**/**    //***/**     /**/********\n",
+            "\t//      ///   ///////     //////      //      ///////  //     // //      /// //      // ////////\n"
+        };
+
+        public Game()
+        {
+            Menu = 0;
+            Choice = 0;
+            IsPlaying = true;
+            CurrentLocation = null;
+        }
+
+        //public static void InitHeroIventory();
+
+        //public static void InitHeroJournal();
+
+        public static void Pause()
+        {
+            Console.Write("\tPress any key...");
+            Console.ReadKey();
+        }
+
+        public void Run()
+        {
+            while (IsPlaying)
+            {
+                Welcome();
+                //WriteLogo();
+                LoadLogo();
+                MainMenu();
+                End();
+            }
+        }
+
+        public void InitAll()
+        {
+            InitLocations();
+        }
+
+
+        public static void Welcome()
+        {
+            Console.Clear();
+            Thread.Sleep(500);
+            Display.Write("\n\tRadosław 'Doic' Michalak presents", 40);
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        public static void WriteLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+
+            foreach (string s in Logo)
+            {
+                Display.Write(s, 1);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public void MainMenu()
+        {
+            Console.ResetColor();
+            Console.WriteLine();
+
+            var options = new Dictionary<string, Action>()
+            {
+                { "Nowa gra", NewGame },
+                { "Wczytaj grę", LoadGame },
+                { "Zmień język", ChangeLanguage },
+                { "Autorzy", Credits },
+                { "Wyjdź z gry", EndGame }
+            };
+
+            Menu mainMenu = new(options);
+        }
+
+        public void NewGame()
+        {
+            SaveManager.CreateSave();
+            InitAll();
+            Console.Clear();
+            //SetCurrentLocation(DarkAlley);
+        }
+
+        public void LoadGame()
+        {
+            Console.Clear();
+        }
+
+        public void ChangeLanguage()
+        {
+            GameSettings.CreateConfigFile();
+            GameSettings.LoadDataFromFile(GameSettings.lang);
+            LoadLogo();
+        }
+
+        public void Credits()
+        {
+            Console.Clear();
+        }
+
+        public void LoadLogo()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+
+            foreach (string s in Logo)
+            {
+                Console.Write(s);
+            }
+
+            Console.WriteLine();
+            Console.ResetColor();
+            MainMenu();
+        }
+
+        public void EndGame()
+        {
+            Console.Clear();
+            Display.Write("\tCzy na pewno chcesz zakończyć rozgrywkę?", 25);
+
+            var options = new Dictionary<string, Action>()
+            {
+                { "Tak", End },
+                { "Nie", LoadLogo }
+            };
+
+            Menu quitMenu = new(options);
+        }
+
+        public void End() { IsPlaying = false; }
+
+        public void SetCurrentLocation(Location location)
+        {
+            CurrentLocation = location;
+            CurrentLocation.Events();
+        }
+
+        public void SetWeather(Weather weather) { Weather = weather; }
+
+        public static void InitLocations()
+        {
+            Location DarkAlley = new("Dark alley", null, null);
+
+            //Locations.Add("Dark Alley", DarkAlley);
+        }
+    }
+}
---
+++ src/GameSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Nocturnal.src
+{
+    public enum GameLanguages { EN = 1, PL = 2 }
+
+    public struct ConfigFileData
+    {
+        public string? Username { get; set; }
+        public int Language { get; set; }
+    }
+
+    internal class GameSettings
+    {
+        public static int lang = 0;
+
+        public bool LoadConfigFile()
+        {
+            if (!File.Exists("config.json"))
+                CreateConfigFile();
+            else
+            {
+                string jsonString = File.ReadAllText("config.json");
+                ConfigFileData configFileData = JsonSerializer.Deserialize<ConfigFileData>(jsonString)!;
+                lang = configFileData.Language;
+            }
+
+            LoadDataFromFile(lang);
+
+            return true;
+        }
+
+        public static void CreateConfigFile()
+        {
+            lang = SelectLanguage();
+
+            var configFileData = new ConfigFileData
+            {
+                Username = Environment.UserName,
+                Language = lang
+            };
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(configFileData, options);
+            File.WriteAllText("config.json", jsonString);
+        }
+
+        public static void LoadDataFromFile(int lang)
+        {
+            string fileName = GetFileName(lang);
+            string path = Directory.GetCurrentDirectory() + "\\data\\localisation\\" + fileName + ".json";
+            File.ReadAllText(path);
+        }
+
+        public static int SelectLanguage()
+        {
+            int choice;
+            while (true)
+            {
+                Console.ResetColor();
+                Console.Clear();
+                Console.WriteLine("\t[1] EN");
+                Console.WriteLine("\t[2] PL");
+                Console.Write("\t> ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice is (int)GameLanguages.EN
+                    or (int)GameLanguages.PL)
+                    break;
+                else continue;
+            }
+
+            return choice;
+        }
+
+        private static string GetFileName(int lang)
+        {
+            if (lang is (int)GameLanguages.EN)
+                return "en";
+            return "pl";
+        }
+    }
+}
