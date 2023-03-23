d794f7a60e7dd0b9ad6cf190a339facb8d2f56b9
doicrm <michalakradek2@gmail.com>
2023-03-23 16:09:28 +0100

    Create Location class

---
+++ src/Location.cs
@@ -0,0 +1,18 @@
+﻿namespace Nocturnal.src
+{
+    internal class Location
+    {
+        private string Name { get; set; }
+        private Fraction Occupation { get; set; }
+        public Action Events { get; set; }
+        private bool IsVisited { get; set; }
+
+        public Location (string name, Fraction occupation, Action events)
+        {
+            Name = name;
+            Occupation = occupation;
+            Events = events;
+            IsVisited = false;
+        }
+    }
+}
