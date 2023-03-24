e78d5a701d6b14ad91247d279d317ca74f392a03
doicrm <michalakradek2@gmail.com>
2023-03-24 15:20:23 +0100

    Created globals

---
+++ src/Globals.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Nocturnal.src
+{
+    public static class Globals
+    {
+        public static Game Game = new();
+        public static JObject? JsonReader;
+        public static Dictionary<string, Location> Locations = new Dictionary<string, Location>();
+    }
+}
