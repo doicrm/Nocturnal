8f9a5a6e24b4f0c78fddfd1745809813937559e9
Doic <michalakradek2@gmail.com>
2023-03-27 09:49:07 +0200

    Created item class

---
+++ src/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace Nocturnal.src
+{
+    public enum ItemType { None, Weapon, Clothes, Food, Quest, Written, Misc }
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public string Description { get; set; }
+        public float Value { get; set; }
+    }
+}
