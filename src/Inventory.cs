96043591777a5d9e469e98d7d115d78e73d66f64
Doic <michalakradek2@gmail.com>
2023-03-27 09:48:55 +0200

    Created inventory class

---
+++ src/Inventory.cs
@@ -0,0 +1,44 @@
+﻿namespace Nocturnal.src
+{
+    public class Inventory
+    {
+        public List<Item> Items = new();
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item);
+        }
+
+        public void Show()
+        {
+            if (!Items.Any()) return;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.WriteLine($"\t{i+1}. {Items[i].Name}");
+            }
+        }
+
+        public void Clear()
+        {
+            if (Items.Any())
+            {
+                Items.Clear();
+            }
+        }
+
+        public bool HasItem(Item item)
+        {
+            foreach (Item Item in Items)
+            {
+                if (Item == item) return true;
+            }
+            return false;
+        }
+    }
+}
