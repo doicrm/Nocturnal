0aae5e6e2b7f58b9faa363d247c077ba0a98511a
doicrm <michalakradek2@gmail.com>
2023-06-06 11:04:02 +0200

    deleted game instance from globals

--- src/Globals.cs
+++ src/Globals.cs
@@ -1,13 +1,8 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿namespace Nocturnal.src;
 
-namespace Nocturnal.src
+public static class Globals
 {
-    public static class Globals
-    {
-        public static Game Game = new();
-        public static Player Player = new();
-        public static dynamic? JsonReader;
-        public static Dictionary<string, Location> Locations = new();
-    }
+    public static Player Player = new();
+    public static dynamic? JsonReader;
+    public static Dictionary<string, Location> Locations = new();
 }
