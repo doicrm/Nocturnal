4b2d13c665eb2cca6e777995401c390839b8f2b6
doicrm <michalakradek2@gmail.com>
2023-06-06 11:03:20 +0200

    added constants

---
+++ src/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Nocturnal.src;
+
+public static class Constants
+{
+    public const string DEFAULT_GAME_NAME       = "Nocturnal";
+    public const string DEFAULT_DEMO_VERSION    = "[Demo Build]";
+
+    public const int    DEFAULT_ATTRIBUTE       = 10;
+}
