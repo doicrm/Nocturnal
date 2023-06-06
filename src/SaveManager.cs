8bf871ead9477f0dc7f5980ad1abb87fc77186ca
doicrm <michalakradek2@gmail.com>
2023-06-06 11:07:17 +0200

    Replaced the game instance from Globals with the one from Program class

--- src/SaveManager.cs
+++ src/SaveManager.cs
@@ -1,130 +1,129 @@
-﻿namespace Nocturnal.src
+﻿namespace Nocturnal.src;
+
+public struct SaveData
 {
-    public struct SaveData
+    public string Date;
+    public string Hour;
+    public string Player;
+    public int Chapter;
+    public int Gender;
+    public int Stage;
+};
+
+public class SaveManager
+{
+    private static int SaveNr = 0;
+
+    public static void CreateSave()
     {
-        public string Date;
-        public string Hour;
-        public string Player;
-        public int Chapter;
-        public int Gender;
-        public int Stage;
-    };
+        if (!Directory.Exists("data\\saves"))
+            Directory.CreateDirectory("data\\saves");
 
-    public class SaveManager
+        string path = $"{Directory.GetCurrentDirectory()}\\data\\saves\\save_{SaveNr}.dat";
+
+        if (!File.Exists(path))
+        {
+            using StreamWriter newSave = File.CreateText(path);
+            SaveNr++;
+            newSave.WriteLine(Logger.GetFormattedUtcTimestamp());
+            newSave.WriteLine($"Player::Unknown");
+            newSave.WriteLine($"Sex::Undefined");
+            newSave.WriteLine($"{0}::{1}");
+            newSave.Close();
+        }
+    }
+
+    public static void LoadSave(int nr)
     {
-        private static int SaveNr = 0;
+        //SaveData save;
+        string path = $"{Directory.GetCurrentDirectory()}\\data\\saves\\save_{SaveNr}.dat";
 
-        public static void CreateSave()
+        if (!File.Exists(path))
         {
-            if (!Directory.Exists("data\\saves"))
-                Directory.CreateDirectory("data\\saves");
-
-            string path = $"{Directory.GetCurrentDirectory()}\\data\\saves\\save_{SaveNr}.dat";
-
-            if (!File.Exists(path))
-            {
-                using StreamWriter newSave = File.CreateText(path);
-                SaveNr++;
-                newSave.WriteLine(Logger.GetFormattedUtcTimestamp());
-                newSave.WriteLine($"Player::Unknown");
-                newSave.WriteLine($"Sex::Undefined");
-                newSave.WriteLine($"{0}::{1}");
-                newSave.Close();
-            }
+            Program.Game!.LoadLogo();
+            return;
         }
 
-        public static void LoadSave(int nr)
+        using StreamReader oldSave = File.OpenText(path);
+        string s;
+        while ((s = oldSave.ReadLine()!) != null)
         {
-            //SaveData save;
-            string path = $"{Directory.GetCurrentDirectory()}\\data\\saves\\save_{SaveNr}.dat";
+            Console.WriteLine(s);
+        }
+        oldSave.Close();
 
-            if (!File.Exists(path))
-            {
-                Globals.Game.LoadLogo();
-                return;
-            }
+        //Hero.heroes["Hero"].Name = save.player;
+        //Hero.heroes["Hero"].Sex = save.gender;
+    }
 
-            using StreamReader oldSave = File.OpenText(path);
-            string s;
-            while ((s = oldSave.ReadLine()!) != null)
-            {
-                Console.WriteLine(s);
-            }
-            oldSave.Close();
+    public static void UpdateSave(int saveNr, string player, int sex, int chapter, int stage)
+    {
 
-            //Hero.heroes["Hero"].Name = save.player;
-            //Hero.heroes["Hero"].Sex = save.gender;
+    }
+
+    public static string PrintSex(int sex)
+    {
+        if (sex == Convert.ToInt32(Genders.Male))
+        {
+            return $"{Globals.JsonReader!["SEX.MALE"]}";
+        }
+        else if (sex == Convert.ToInt32(Genders.Female))
+        {
+            return $"{Globals.JsonReader!["SEX.FEMALE"]}";
+        }
+        return $"{Globals.JsonReader!["SEX.UNDEFINED"]}";
+    }
+
+    private static string GetChapterString(int chapter)
+    {
+        if (chapter == 0 || chapter < 0)
+        {
+            return $"{Globals.JsonReader!["PROLOGUE"]}";
+        }
+        else if (chapter == 1 || chapter == 2)
+        {
+            return $"{Globals.JsonReader!["CHAPTER"]} {chapter}";
+        }
+        return $"{Globals.JsonReader!["EPILOGUE"]}";
+    }
+
+    private static void LoadSaveInfo(string saveToLoad)
+    {
+        //SaveData save;
+
+        if (!File.Exists(saveToLoad))
+        {
+            Console.WriteLine($"{saveToLoad} - nie ma takiego pliku!");
+            return;
         }
 
-        public static void UpdateSave(int saveNr, string player, int sex, int chapter, int stage)
+        using StreamReader oldSave = File.OpenText(saveToLoad);
+        string s;
+        while ((s = oldSave.ReadLine()!) != null)
         {
-
+            Console.WriteLine($"\t{s}");
         }
+        oldSave.Close();
 
-        public static string PrintSex(int sex)
+        //Console.WriteLine($"\t{save.Player}, {PrintSex(save.Gender)} | {GetChapterString(save.Chapter)} : {save.Stage} | {save.Date} {save.Hour}");
+    }
+
+    public static void SearchForSaves()
+    {
+        string path = $"{Directory.GetCurrentDirectory()}\\data\\saves";
+        var files = Directory.GetFiles(path, "save_*", SearchOption.AllDirectories)
+        .Where(s => s.EndsWith(".dat"));
+
+        if (files.Any())
         {
-            if (sex == Convert.ToInt32(Genders.Male))
-            {
-                return $"{Globals.JsonReader!["SEX.MALE"]}";
-            }
-            else if (sex == Convert.ToInt32(Genders.Female))
-            {
-                return $"{Globals.JsonReader!["SEX.FEMALE"]}";
-            }
-            return $"{Globals.JsonReader!["SEX.UNDEFINED"]}";
+            foreach (dynamic file in files)
+                LoadSaveInfo(file);
         }
-
-        private static string GetChapterString(int chapter)
+        else
         {
-            if (chapter == 0 || chapter < 0)
-            {
-                return $"{Globals.JsonReader!["PROLOGUE"]}";
-            }
-            else if (chapter == 1 || chapter == 2)
-            {
-                return $"{Globals.JsonReader!["CHAPTER"]} {chapter}";
-            }
-            return $"{Globals.JsonReader!["EPILOGUE"]}";
-        }
-
-        private static void LoadSaveInfo(string saveToLoad)
-        {
-            //SaveData save;
-
-            if (!File.Exists(saveToLoad))
-            {
-                Console.WriteLine($"{saveToLoad} - nie ma takiego pliku!");
-                return;
-            }
-
-            using StreamReader oldSave = File.OpenText(saveToLoad);
-            string s;
-            while ((s = oldSave.ReadLine()!) != null)
-            {
-                Console.WriteLine($"\t{s}");
-            }
-            oldSave.Close();
-
-            //Console.WriteLine($"\t{save.Player}, {PrintSex(save.Gender)} | {GetChapterString(save.Chapter)} : {save.Stage} | {save.Date} {save.Hour}");
-        }
-
-        public static void SearchForSaves()
-        {
-            string path = $"{Directory.GetCurrentDirectory()}\\data\\saves";
-            var files = Directory.GetFiles(path, "save_*", SearchOption.AllDirectories)
-            .Where(s => s.EndsWith(".dat"));
-
-            if (files.Any())
-            {
-                foreach (dynamic file in files)
-                    LoadSaveInfo(file);
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"{Globals.JsonReader!["NO_SAVES_FOUND"]}");
-                Console.ResetColor();
-            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"{Globals.JsonReader!["NO_SAVES_FOUND"]}");
+            Console.ResetColor();
         }
     }
 }
