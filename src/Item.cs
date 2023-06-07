6ce26d12c690ed80874f2c1a4ece523c24e97937
doicrm <michalakradek2@gmail.com>
2023-06-07 12:28:43 +0200

    Moved Weapon class to another file

--- src/Item.cs
+++ src/Item.cs
@@ -17,26 +17,3 @@ public abstract class Item
             $"{Globals.JsonReader!["VALUE"]}: {Value}");
     }
 }
-
-public class Weapon : Item
-{
-    public int DamageMin { get; set; }
-    public int DamageMax { get; set; }
-
-    public Weapon (int damageMin, int damageMax)
-    {
-        Type = ItemType.Weapon;
-        DamageMin = damageMin;
-        DamageMax = damageMax;
-    }
-
-    public override string PrintInfo()
-    {
-        return ($"{Globals.JsonReader!["NAME"]}: {Name}\n" +
-            $"{Globals.JsonReader!["DESCRIPTION"]}: {Description}\n" +
-            $"{Globals.JsonReader!["DAMAGE_MIN"]}: {DamageMin}\n" +
-            $"{Globals.JsonReader!["DAMAGE_MAX"]}: {DamageMin}\n" +
-            $"{Globals.JsonReader!["TYPE"]}: {Type}\n" +
-            $"{Globals.JsonReader!["VALUE"]}: {Value}");
-    }
-}
---
+++ src/Weapon.cs
@@ -0,0 +1,25 @@
+﻿namespace Nocturnal.src;
+
+public class Weapon : Item
+{
+    public int DamageMin { get; set; }
+    public int DamageMax { get; set; }
+
+    public Weapon(int damageMin, int damageMax)
+    {
+        Type = ItemType.Weapon;
+        DamageMin = damageMin;
+        DamageMax = damageMax;
+    }
+
+    public override string PrintInfo()
+    {
+        return ($"{Globals.JsonReader!["NAME"]}: {Name}\n" +
+            $"{Globals.JsonReader!["DESCRIPTION"]}: {Description}\n" +
+            $"{Globals.JsonReader!["DAMAGE_MIN"]}: {DamageMin}\n" +
+            $"{Globals.JsonReader!["DAMAGE_MAX"]}: {DamageMin}\n" +
+            $"{Globals.JsonReader!["TYPE"]}: {Type}\n" +
+            $"{Globals.JsonReader!["VALUE"]}: {Value}");
+    }
+}
+
